6ae2400d61efb6ed115c6592fb6b2187393253e8
IvanNavarreteTepozan <12ivanavarrete@gmail.com>
2025-05-13 20:47:10 -0600

    Delete javascript directory

--- javascript/FormularioDinamico.js
+++
@@ -1,4 +0,0 @@
-function cambiarFormulario(mostrado, ocultado) {
-            document.getElementById(ocultado).classList.add("hidden"); // Oculta la parte anterior
-            document.getElementById(mostrado).classList.remove("hidden"); // Muestra la nueva parte
-        }
--- javascript/Frames.js
+++
@@ -1,8 +0,0 @@
-function abrirEnIframe(url) {
-    let iframe = document.querySelector("iframe");
-    if (iframe) {
-        iframe.src = url;
-    } else {
-        console.error("No se encontró un iframe en la página.");
-    }
-}
